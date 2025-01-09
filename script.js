1c27d0aefae753deb31860b216d4b33aca4cbeaf
Samuel Dippert <samueldippert04@gmail.com>
2025-01-09 19:14:48 +0100

    Format changes

--- script.js
+++ script.js
@@ -4,42 +4,43 @@ let numbersInt = [];
 let operators = [];
 
 //Function saving all pressed buttons and showing them on screen
-function button(x){
+function button(x) {
     display = display + x;
     document.getElementById("monitor").innerHTML = display;
 }
 
 //Function sorting user input by numbers and operators
-function sortInput(){
+function sortInput() {
     let num = "";
-    for (let i = 0; i < display.length; i++){
+    for (let i = 0; i < display.length; i++) {
         const char = display[i];
-        if (!isNaN(char)){
+        if (!isNaN(char)) {
             num = num + char;
         } else {
-            if (num !== ""){
-            numbersString.push(num);
-            num = "";
+            if (num !== "") {
+                numbersString.push(num);
+                num = "";
+            }
+            operators.push(char);
         }
-        operators.push(char);
+        numbersInt = numbersString.map(Number);
+        calculate();
     }
-    numbersInt = numbersString.map(Number);
-    calculate();
-}}
+}
 
-function calculate(){
-    
+function calculate() {
+
 
     document.getElementById("monitor").innerHTML = result
 }
 
-function printt(){
+function printt() {
     console.log(numbersString);
     console.log(numbersInt);
     console.log(operators);
 }
 
-function reset(){
+function reset() {
     display = "";
     operators = [];
     numbersString = [];
