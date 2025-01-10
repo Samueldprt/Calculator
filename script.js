52324d8d147dc9c9043c035dc1167eb552e23add
Samuel Dippert <samueldippert04@gmail.com>
2025-01-10 22:51:32 +0100

    Further Front and Backend Developement

--- script.js
+++ script.js
@@ -3,13 +3,47 @@ let numbersString = [];
 let numbersInt = [];
 let operators = [];
 let result = 0;
-let ans = 120;
+let ansValue = 0;
+let bracketSwitch = false;
 
 //Function saving all pressed buttons and showing them on screen
 function button(x) {
-    display = display + x;
+    const allOperators = ["+", "-", "*", "/"];
+
+    if (isNaN(x)) {
+        if (allOperators.includes(display.charAt(display.length - 1))) {
+            display = display.slice(0, -1) + x;
+        } else {
+            display += x;
+        }
+    } else {
+        display += x;
+    }
     document.getElementById("monitor").innerHTML = display;
 }
+
+function erase() {
+    display = display.slice(0, -1)
+    document.getElementById("monitor").innerHTML = display;
+}
+
+function ans() {
+    display += ansValue;
+    document.getElementById('monitor').innerHTML = display;
+}
+
+function brackets() {
+    let bracket;
+    if (!bracketSwitch) {
+        bracket = "("
+    } else {
+        bracket = ")"
+    };
+    bracketSwitch = !bracketSwitch;
+    display += bracket;
+    document.getElementById('monitor').innerHTML = display;
+}
+
 
 //Function sorting user input by numbers and operators
 function sortInput() {
@@ -19,19 +53,26 @@ function sortInput() {
         if (!isNaN(char)) {
             num = num + char;
         } else {
-            if (num !== "") {
-                numbersString.push(num);
-                num = "";
+            if (char == ".") {
+                num = num + char;
+            } else {
+                if (num !== "") {
+                    numbersString.push(num);
+                    num = "";
+                }
+                operators.push(char);
             }
-            operators.push(char);
         }
-        numbersInt = numbersString.map(Number);
-        calculate();
     }
+    if (num !== "") {
+        numbersString.push(num);
+    }
+    numbersInt = numbersString.map(Number);
+    calculate();
 }
 
 function calculate() {
-    if (display[0] == "-") {
+    if (display[0] == "-") { //calculate beginning with a negative number
         result = numbersInt[0] * -1;
         for (let i = 0; i < operators.length; i++) {
             const operator = operators[i + 1];
@@ -40,31 +81,73 @@ function calculate() {
             switch (operator) {
                 case "+":
                     result += nextNumber;
-
+                    break;
                 case "-":
                     result -= nextNumber;
-
+                    break;
                 case "*":
                     result *= nextNumber;
+                    break;
+                case "/":
+                    if (nextNumber === 0) {
+                        document.getElementById("monitor").innerHTML = "Div/0 Fehler";
+                        return;
+                    }
+                    result /= nextNumber;
+                    break;
             }
         }
 
-    } else {
+    } else { //calculate beginning with a positiv number   
         result = numbersInt[0];
         for (let i = 0; i < operators.length; i++) {
             const operator = operators[i];
             const nextNumber = numbersInt[i + 1];
 
+            switch (operator) {
+                case "+":
+                    result += nextNumber;
+                    break;
+
+                case "-":
+                    result -= nextNumber;
+                    break;
+
+                case "*":
+                    result *= nextNumber;
+                    break;
+
+                case "/":
+                    if (nextNumber === 0) {
+                        document.getElementById("monitor").innerHTML = "Div/0 Fehler";
+                        return;
+                    }
+                    result /= nextNumber;
+                    break;
+            }
 
         }
-
     }
 
-    document.getElementById("monitor").innerHTML = result;
-    ans = result;
+    if (isNaN(result)) {
+        result = "0"
+        document.getElementById("monitor").innerHTML = "Syntax Error";
+        display = ""
+        operators = []; //reset values for new calculation
+        numbersString = []; //
+        numbersInt = []; //
+
+    } else {
+        document.getElementById("monitor").innerHTML = result;
+        display = result.toString();
+        ansValue = result;
+        operators = []; //reset values for new calculation
+        numbersString = []; //
+        numbersInt = []; //
+    }
 }
 
-function printt() {
+function debug() {
     console.log(numbersString);
     console.log(numbersInt);
     console.log(operators);
@@ -77,5 +160,6 @@ function ac() {
     numbersInt = [];
     result = "";
     document.getElementById("monitor").innerHTML = display;
+    bracketSwitch = false;
 }
 
